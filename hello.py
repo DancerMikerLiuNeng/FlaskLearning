731cb0bd8f10e4758a9a91dbaf94a609226886a7
hikki <359373468@qq.com>
2018-04-09 22:13:28 +0800

    add 404 ,500 error handler html and route , favico.ico

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 from flask import Flask,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def pag_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/user/<name>')
 def user(name):
