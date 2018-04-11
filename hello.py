6731f18e5f1d32d904432d89a92d829f4d245d19
hikki <359373468@qq.com>
2018-04-11 20:25:24 +0800

    add flask wtforms

--- hello.py
+++ hello.py
@@ -1,20 +1,36 @@
 from flask import Flask,render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'guess'
 bootstrap = Bootstrap(app)
+mement = Moment(app)
 manager = Manager(app)
-@app.route('/')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',current_time=datetime.utcnow(),form=form,name=name)
 
 @app.errorhandler(404)
 def pag_not_found(e):
     return render_template('404.html'), 404
 
-@app.errorhandler
+@app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
@@ -24,4 +40,5 @@ def user(name):
     return render_template('user.html', name=name)
 
 if __name__=='__main__':
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
