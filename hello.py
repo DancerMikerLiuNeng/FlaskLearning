14ae0bf86bc31979dcb32e1bb2539d8ade1793cd
hikki <359373468@qq.com>
2018-04-15 21:37:12 +0800

    add user data to database

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ app.config['SECRET_KEY'] = 'guess'
 app.config['SQLALCHEMY_DATABASE_URI']=\
 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 mement = Moment(app)
@@ -25,7 +26,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name  = db.Column(db.String(64), unique=True)
-
+    users = db.relationship('User', backref='role', lazy='dynamic')
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -33,7 +34,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -47,13 +48,27 @@ def index():
     # name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
+        # old_name = session.get('name')
+        # if old_name is not None and old_name != form.name.data:
+        #     flash('Looks like you have changed your name!')
+        # session['name'] = form.name.data
+        # return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
             flash('Looks like you have changed your name!')
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',current_time=datetime.utcnow(),
-                           form=form,name=session.get('name'))
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                           form=form,
+                           name=session.get('name'),
+                           known=session.get('known',False))
 
 @app.errorhandler(404)
 def pag_not_found(e):
